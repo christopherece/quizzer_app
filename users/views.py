26289de7f591740562c4a11ea7299366a0033825
christopherece <christopheranchetaece@gmail.com>
2024-03-16 21:12:53 +1300

    Added Disabled if no time scheduled2

--- users/views.py
+++ users/views.py
@@ -42,8 +42,7 @@ def profiles(request):
     # categories = Category.objects.prefetch_related('subcategory_set', 'question_set__option_set').all()
     # categories = Category.objects.filter(is_active=True, created_by=request.user).prefetch_related('subcategory_set', 'question_set__option_set')
     categories = Category.objects.filter(is_active=True).prefetch_related('subcategory_set', 'question_set__option_set')
-    subcategories = Subcategory.objects.values('name','creation_date')
-    studstats = StudentStats.objects.filter(user=request.user)
+    subcategories = Subcategory.objects.values('name','creation_date').order_by('is_active', 'name')
     context = {
         'categories': categories,
         'studstats':studstats,
