9b4f50ba4966daf48392e5237ca9c1286c900068
christopherece <christopheranchetaece@gmail.com>
2024-03-19 23:18:53 +1300

    Added Teacher Stat Page

--- teachers/views.py
+++ teachers/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from users.models import StudentStats
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+from datetime import datetime, timezone
+
+@login_required(login_url='loginUser')
 def index(request):
-    student_stats = StudentStats.objects.all()
+    student_stats = StudentStats.objects.filter(subcategory__created_by=request.user)
     context = {
-        'student_stats': student_stats
+        'student_stats': student_stats,
     }
     return render(request, 'teachers/index.html', context)
