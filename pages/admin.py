d9c81d7f689628a32a145e91ca98e67d03976c4c
christopherece <christopheranchetaece@gmail.com>
2024-03-20 23:03:39 +1300

    Modified Admin Page

--- pages/admin.py
+++ pages/admin.py
@@ -15,21 +15,64 @@ class SubcategoryInline(admin.TabularInline):
     extra = 1
     
 class SubcategoryAdmin(admin.ModelAdmin):
-    list_display = ('name','category','is_active')
+    list_display = ('name','category','is_active', 'created_by_name')
     list_editable = ['is_active']
+
+    def created_by_name(self, obj):
+        return obj.created_by.profile.name if obj.created_by.profile else None
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:  # Superuser can see all subcategories
+            return qs
+        return qs.filter(created_by=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by_id:  # If created_by is a ForeignKey to User model
+            obj.created_by = request.user
+        obj.save()
+
+    def add_view(self, request, form_url='', extra_context=None):
+        data = request.GET.copy()
+        data.update({'created_by': request.user.pk})  # Set the default created_by
+        request.GET = data
+        return super().add_view(request, form_url, extra_context)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [SubcategoryInline]
-    # list_filter = ('name','created_by','is_active')
     list_display = ('name','created_by','is_active')
     list_editable = ['is_active']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:  # Superuser can see all categories
+            return qs
+        return qs.filter(created_by=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by_id:  # If created_by is a ForeignKey to User model
+            obj.created_by = request.user
+        obj.save()
+
+    def add_view(self, request, form_url='', extra_context=None):
+        data = request.GET.copy()
+        data.update({'created_by': request.user.pk})  # Set the default created_by
+        request.GET = data
+        return super().add_view(request, form_url, extra_context)
 
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [OptionInline]
     list_display = ('text','category','subcategory')
     list_filter = ('category','subcategory')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            kwargs["queryset"] = Category.objects.filter(created_by=request.user)
+        elif db_field.name == "subcategory":
+            kwargs["queryset"] = Subcategory.objects.filter(created_by=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
